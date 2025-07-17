b2dcdf22dd6837d7e7569750d7821230c0629cec
Muhammetcoskun07 <mc522848@gmail.com>
2025-07-17 16:35:32 +0300

    Create Conversation, FileStorage, Message

---
+++ SesliDil.Core/Entities/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SesliDil.Core.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public int ThreadId { get; set; }
+        public string Role { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+
+        public Conversation Conversation { get; set; }
+    }
+
+}
