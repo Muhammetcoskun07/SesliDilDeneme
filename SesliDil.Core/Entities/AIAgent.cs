b4c2985a28f12b1f4bcf281e5b9bfa569e3fca0c
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-07-17 16:04:12 +0300

    AddedProgressAndAIgentEntitiesAddCore

---
+++ SesliDil.Core/Entities/AIAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SesliDil.Core.Entities
+{
+    public class AIAgent
+    {
+        public int AgentId { get; set; }
+        public string AgentName { get; set; }
+        public string AgentPrompt { get; set; }
+
+    }
+}
---
+++ SesliDil.Core/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SesliDil.Core.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Language { get; set; }
+        public string Interests { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
