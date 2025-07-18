19c2b7da897151a9171ef31c2dfca2e57a5e3934
Muhammetcoskun07 <mc522848@gmail.com>
2025-07-18 12:14:12 +0300

    repository.cs düzenlendi

--- SesliDil.Data/Repositories/Repository.cs
+++ SesliDil.Data/Repositories/Repository.cs
@@ -1,50 +1,50 @@
-﻿using System;
+﻿using SesliDil.Core.Interfaces;
+using SesliDil.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SesliDil.Data.Repositories
 {
-    //public class Repository<T> : IRepository<T> where T : class
-    //{
-    //    private readonly AppDbContext _context;
-    //    private readonly DbSet<T> _dbSet;
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SesliDilDbContext _context;
+        private readonly DbSet<T> _dbSet;
 
-    //    public Repository(AppDbContext context)
-    //    {
-    //        _context = context;
-    //        _dbSet = _context.Set<T>();
-    //    }
+        public Repository(SesliDilDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
 
-    //    public async Task<List<T>> GetAllAsync()
-    //    {
-    //        return await _dbSet.ToListAsync();
-    //    }
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
 
-    //    public async Task<T> GetByIdAsync(int id)
-    //    {
-    //        return await _dbSet.FindAsync(id);
-    //    }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
 
-    //    public async Task AddAsync(T entity)
-    //    {
-    //        await _dbSet.AddAsync(entity);
-    //    }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
 
-    //    public void Update(T entity)
-    //    {
-    //        _dbSet.Update(entity);
-    //    }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
 
-    //    public void Delete(T entity)
-    //    {
-    //        _dbSet.Remove(entity);
-    //    }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
 
-    //    public async Task SaveChangesAsync()
-    //    {
-    //        await _context.SaveChangesAsync();
-    //    }
-    //}
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
 }
