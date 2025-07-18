623a989a0ea3539ea9213b07b95ba2e4b6ddda93
Muhammetcoskun07 <mc522848@gmail.com>
2025-07-18 10:57:29 +0300

    Generic repository arayüzü eklendi, AIAgentRepository kaldırıldı

--- SesliDil.Core/Interfaces/IRepository.cs
+++ SesliDil.Core/Interfaces/IRepository.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace SesliDil.Core.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
-
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
     }
 }
