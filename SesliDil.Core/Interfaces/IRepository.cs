a32a2787f5e2de1d43f4d40be0e14e1b6fd3aa7e
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-07-30 14:11:05 +0300

    updateuserservice

--- SesliDil.Core/Interfaces/IRepository.cs
+++ SesliDil.Core/Interfaces/IRepository.cs
@@ -8,11 +8,12 @@ namespace SesliDil.Core.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync<TId>(TId id);
+        Task<IEnumerable<T>> GetAllAsync();
         Task AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
-        Task SaveChangesAsync();
+        Task<int> SaveChangesAsync(); 
+        IQueryable<T> Query();
     }
 }
