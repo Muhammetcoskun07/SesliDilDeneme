1339bce5b61b7df9c33b3b0ea5bd521240262965
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-08-06 11:15:25 +0300

    addsummarymigration

---
+++ SesliDil.Data/Migrations/20250806081301_addsummary.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SesliDil.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsummary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Summary",
+                table: "Conversation",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Summary",
+                table: "Conversation");
+        }
+    }
+}
