41c90328f757aac9059c4fb7dc95d689af471bb7
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-08-06 17:54:58 +0300

    addedvalidator

---
+++ SesliDilDeneme.API/Validators/SendMessageRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace SesliDilDeneme.API.Validators
+{
+    public class SendMessageRequestValidator : AbstractValidator<SendMessageRequest>
+    {
+        public SendMessageRequestValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Content is required");
+
+            RuleFor(x => x.AgentId)
+                .NotEmpty().WithMessage("AgentId is required");
+
+        }
+    }
+   
+}
