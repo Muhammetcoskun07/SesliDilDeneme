e7d31cc427274df634a95891b90bd2a383e9679a
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-08-11 13:34:03 +0300

    addedupdatelearningpreferences

---
+++ SesliDil.Core/DTOs/UpdateUserPreferencesDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SesliDil.Core.DTOs
+{
+    public class UpdateUserPreferencesDto
+    {
+        public JsonDocument? LearningGoals { get; set; }
+        public JsonDocument? ImprovementGoals { get; set; }
+        public JsonDocument? TopicInterests { get; set; }
+        public string? WeeklySpeakingGoal { get; set; }
+    }
+}
