ce146a5c977b0cd82f7b84d0765d373bcc6b64f0
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-08-11 18:25:30 +0300

    addedcorrextedtextmigration

---
+++ SesliDil.Data/Migrations/20250811152448_addedcorrectedtext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SesliDil.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedcorrectedtext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CorrectedText",
+                table: "Message",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectedText",
+                table: "Message");
+        }
+    }
+}
