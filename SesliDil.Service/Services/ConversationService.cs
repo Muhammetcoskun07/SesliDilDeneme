d947160a1164269036b51f69f5b7667d55d48ab5
sumeyyekoyuncu <sumeyye.koyuncu@icloud.com>
2025-08-15 17:12:27 +0300

    -

--- SesliDil.Service/Services/ConversationService.cs
+++ SesliDil.Service/Services/ConversationService.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;   // <-- ToListAsync(), EF.Functions, ExecuteSqlRawAsync için
+using System.Linq;
 using SesliDil.Core.DTOs;
 using SesliDil.Core.Entities;
 using SesliDil.Core.Interfaces;
